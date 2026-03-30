1e6773b4d728538f9703b0f238d906a4ede35f40
mason <58895787+masoniis@users.noreply.github.com>
2026-03-30 13:09:34 -0600

    ci: set up flake installatin workflow and autoreleases

--- flake.nix
+++ flake.nix
@@ -9,78 +9,69 @@
     systems.url = "github:nix-systems/default";
     utils.url = "github:numtide/flake-utils";
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-unstable,
-      utils,
-      treefmt-nix,
-      systems,
-    }:
-    utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, nixpkgs-unstable, utils, treefmt-nix, systems, }:
+    utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
-      in
-      {
-        devShell =
-          with pkgs;
-          mkShell (
-            {
-              buildInputs = [
-                # rust stuff
-                pkgs-unstable.tracy-glfw
-                pkgs-unstable.rust-analyzer
-                pkgs-unstable.wgsl-analyzer
-                rustup
+      in {
+        # INFO: ------------------------
+        #         local devshell
+        # ------------------------------
 
-                # utils
-                just
-                ripgrep # for justfile
-                gnuplot # for benchmarks
-              ]
-              ++ (lib.optionals stdenv.isLinux [
-                libGL
-                libxkbcommon
-                wayland
-                pkg-config
-                mesa
-              ]);
+        devShell = with pkgs;
+          mkShell ({
+            buildInputs = [
+              # rust stuff
+              pkgs-unstable.tracy-glfw
+              pkgs-unstable.rust-analyzer
+              pkgs-unstable.wgsl-analyzer
+              rustup
 
-              shellHook = ''
-                export PATH="$HOME/.cargo/bin:$PATH"
-                rustup show active-toolchain
-              '';
+              # utils
+              just
+              ripgrep # for justfile
+              gnuplot # for benchmarks
+            ] ++ (lib.optionals stdenv.isLinux [
+              libGL
+              libxkbcommon
+              wayland
+              pkg-config
+              mesa
+            ]);
 
-            }
-            // (lib.optionalAttrs stdenv.isLinux {
-              # untested but probably not needed now that using rustup instead of cargo from nixpkgs
-              # RUST_SRC_PATH = "${pkgs-unstable.rust.packages.stable.rustPlatform.rustLibSrc}";
+            shellHook = ''
+              export PATH="$HOME/.cargo/bin:$PATH"
+              rustup show active-toolchain
+            '';
 
-              LD_LIBRARY_PATH = lib.makeLibraryPath [
-                libGL
-                libxkbcommon
-                wayland
-              ];
-            })
-          );
-      }
-    )
-    // (
-      let
+          } // (lib.optionalAttrs stdenv.isLinux {
+            LD_LIBRARY_PATH =
+              lib.makeLibraryPath [ libGL libxkbcommon wayland ];
+          }));
+
+        # INFO: -------------------------
+        #         CI package sets
+        # -------------------------------
+
+        packages.default = pkgs.buildEnv {
+          name = "gh action empty default profile";
+          paths = [ ];
+        };
+      }) // (let
         # iterate each system and evaluate
-        eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-        treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
-      in
-      {
+        eachSystem = f:
+          nixpkgs.lib.genAttrs (import systems)
+          (system: f nixpkgs.legacyPackages.${system});
+        treefmtEval =
+          eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+      in {
         # for `nix fmt`
-        formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+        formatter =
+          eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
         # for `nix flake check`
         checks = eachSystem (pkgs: {
           formatting = treefmtEval.${pkgs.system}.config.build.check self;
         });
-      }
-    );
+      });
 }
